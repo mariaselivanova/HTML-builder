1ac205ac8d9972b24bc296bfac7d46e3d1c5caf6
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-05-03 01:45:30 -0400

    feat: add 1st task solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const reader = fs.createReadStream(path.join(__dirname, 'text.txt'), { encoding: 'utf8' });
+
+reader.on('data', (chunk) => {
+    process.stdout.write(chunk);
+});
